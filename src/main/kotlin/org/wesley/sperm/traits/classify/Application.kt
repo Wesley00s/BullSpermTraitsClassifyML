4473f02e8c978bde4e41eb2e6a947d7e176ea5d1
Wesley R. S <wesley300rodrigues@gmail.com>
2024-05-04 14:14:40 -0300

    npm install

--- src/main/kotlin/org/wesley/sperm/traits/classify/Application.kt
+++ src/main/kotlin/org/wesley/sperm/traits/classify/Application.kt
@@ -12,9 +12,17 @@ class SpermTraitsClassifyApplication
 fun main(args: Array<String>) {
     runApplication<SpermTraitsClassifyApplication>(*args)
     val pathFrontend = "src/main/react/sperm-bulls-classify-frontend"
-    val process = ProcessBuilder("npm", "start")
+    val npmInstallProcess = ProcessBuilder("npm", "install")
         .directory(File(pathFrontend))
         .inheritIO()
         .start()
-    process.waitFor()
+
+    npmInstallProcess.waitFor()
+
+    val npmStartProcess = ProcessBuilder("npm", "start")
+        .directory(File(pathFrontend))
+        .inheritIO()
+        .start()
+
+    npmStartProcess.waitFor()
 }
